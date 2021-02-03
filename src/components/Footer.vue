5f97ec4adad35791ab3a155e19ec863aa67f0816
wangxiaoyang <15847424609@163.com>
2021-02-03 21:46:32 +0800

    feat: 新增全局底部组件

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="copyright">
+        Copyright Linter. All Rights Reserved
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style>
+.copyright {
+    color: #909399;
+    font-size: 14px;
+    text-align: center;
+}
+</style>
