fec51f3a2d41f8e6193154c2ae40f7b53efa6715
citrucn <1948288473@qq.com>
2021-06-01 21:11:43 +0800

    fix: 修正课程表单校验规则

--- src/components/Course-Form.vue
+++ src/components/Course-Form.vue
@@ -58,7 +58,7 @@ export default {
             rules: {
                 name: [
                     {required: true, message: '请输入名称', trigger: 'blur'},
-                    {min: 2, max: 20, message: '长度在 2 到 20 个字符', trigger: 'blur'}
+                    {min: 2, max: 100, message: '长度在 2 到 100 个字符', trigger: 'blur'}
                 ],
                 description: [
                     {required: true, message: '请输入简介', trigger: 'blur'},
