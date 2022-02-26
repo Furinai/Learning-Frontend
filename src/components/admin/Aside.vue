3d545f35cf02bd5e5a2297676fb0714d0dfc784a
wangxiaoyang <1948288473@qq.com>
2022-02-26 20:37:31 +0800

    :bug: 修正子菜单标签名称

--- src/components/admin/Aside.vue
+++ src/components/admin/Aside.vue
@@ -6,23 +6,23 @@
                     <i class="el-icon-house"></i>首页
                 </template>
             </el-menu-item>
-            <el-submenu index="1">
+            <el-sub-menu index="1">
                 <template #title>
                     <i class="el-icon-collection-tag"></i>分类
                 </template>
                 <el-menu-item index="1-1" :route="{name: 'Category-Manage'}">
                     分类管理
                 </el-menu-item>
-            </el-submenu>
-            <el-submenu index="2">
+            </el-sub-menu>
+            <el-sub-menu index="2">
                 <template #title>
                     <i class="el-icon-reading"></i>课程
                 </template>
                 <el-menu-item index="2-1" :route="{name: 'Course-Manage'}">
                     课程管理
                 </el-menu-item>
-            </el-submenu>
-            <el-submenu index="3">
+            </el-sub-menu>
+            <el-sub-menu index="3">
                 <template #title>
                     <i class="el-icon-user"></i>用户
                 </template>
@@ -32,7 +32,7 @@
                 <el-menu-item index="3-2" :route="{name: 'Role-Manage'}">
                     角色管理
                 </el-menu-item>
-            </el-submenu>
+            </el-sub-menu>
         </el-scrollbar>
     </el-menu>
 </template>
